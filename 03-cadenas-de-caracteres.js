dd2c4cbe9304b3f07a6249e527614f7ab3c62bf9
Luis Guzman <luisguzman1988@gmail.com>
2021-12-18 09:42:40 -0400

    sumando mas contenido al curso

---
+++ 03-cadenas-de-caracteres.js
@@ -0,0 +1,18 @@
+//     con esta barra podemos escapar de las comilla dobles\
+
+// dentrod de una cadena no pueden haber dos comillas simples, se puede usar una doble y las demas simples o viceversa
+
+
+// Secuencia de escape
+
+// \b (solo en cadenas, no en expresiones regulares)	retroceso	U + 0008
+// \t	pestaña horizontal	U + 0009
+// \n	linea de alimentación	U + 000A
+// \v	pestaña vertical	U + 000B
+// \f	form feed	U + 000C
+// \r	retorno de carro	U + 000D
+
+var programacion = 'Programacion orintada a objeto';
+
+document.write(programacion.length)
+document.write(programacion[1])
