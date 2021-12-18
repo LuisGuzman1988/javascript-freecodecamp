550db92ff4b6bbd5fc2af93d7a4cef0d0dfa6dbd
Luis Guzman <luisguzman1988@gmail.com>
2021-12-18 08:48:32 -0400

    operadores y incremto

---
+++ 02-operadores.js
@@ -0,0 +1,18 @@
+/*
+* Operaciones Aritmeticas Básicas
+
+Suma Resta Multiplicación División
+
+*/
+
+var a = 2;
+var b = 3;
+var totalSuma = a + b;
+var totalResta = a - b;
+var totalMultiplicacion = a * b;
+var totalDivision = a / b;
+
+console.log(totalSuma);
+console.log(totalResta);
+console.log(totalMultiplicacion);
+console.log(totalDivision);
---
+++ 03-incremento.js
@@ -0,0 +1,33 @@
+/*
+Incremento de las variable ++
+Decremento de las variables --
+
+*/
+
+var librosComprados = 100;
+//console.log(librosComprados);
+
+// Opción 1
+
+librosComprados = librosComprados + 1;
+
+// Opción 2
+
+librosComprados++;
+console.log(librosComprados);
+
+// Operadores de Asignacion Compusesto
+
+// += -+ *=
+
+var a = 15;
+
+//Opcion 1
+
+a = a + 5;
+
+//Opcion 2
+
+a += 5;
+
+console.log(a)
